205ff334252dc8d1d499701c0efc9f5d3965c12c
jeonguk29 <jeonguk29@naver.com>
2023-07-17 17:21:12 +0900

    파이어베이스 코드 리팩토링, FirebaseStorage에 사용자 프로필 이미지를 저장하고 해당 url을 FirebaseDatabase사용자 정보에 저장

--- iOS_Twitter/iOS_Twitter/Controller/Authentication/RegistrationController.swift
+++ iOS_Twitter/iOS_Twitter/Controller/Authentication/RegistrationController.swift
@@ -122,32 +122,44 @@ class RegistrationController: UIViewController {
         guard let fullname = fullnameTextField.text else {return}
         guard let username = usernameTextField.text else {return}
         
-        // 파이어베이스의 사용자를 생성
-        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
-            // iOS에서 completion 매개변수는 비동기 작업이 완료된 후 실행될 코드 블럭을 지정하는 매개변수임
-            // 일반적으로 비동기 작업이 완료된 후 실행할 코드를 전달하기 위해 사용 ex API 호출
-            if let error = error {
-                print("DEBUG:  Error is \(error.localizedDescription)")
-                return
+        // 사용자 이미지를 저장할때 파일이름을 우리가 생성하고 전달 해야함 
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else {return}
+        let filename = NSUUID().uuidString // 파일 이름 직접 생성하기 위함
+        let storageRef = STORAGE_PROFILE_IMAGE.child(filename)
+       
+        storageRef.putData(imageData) { (mata, error) in
+            // 다운로드 URL를 받아야함
+            storageRef.downloadURL { (url, error)in
+                guard let profileImageUrl = url?.absoluteString else {return}
+                
+                // 파이어베이스의 사용자를 생성
+                Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                    // iOS에서 completion 매개변수는 비동기 작업이 완료된 후 실행될 코드 블럭을 지정하는 매개변수임
+                    // 일반적으로 비동기 작업이 완료된 후 실행할 코드를 전달하기 위해 사용 ex API 호출
+                    if let error = error {
+                        print("DEBUG:  Error is \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    print("성공적으로 사용자 등록")
+                    // 성공시 사용자에게 할당되는 고유의 uid를 가져옴, 데이터베이스에 저장할때 사용자 고유의 데이터를 관리하기 위함임
+                    guard let uid = result?.user.uid else {return}
+                    
+                    let values = ["email" : email,
+                                  "username" : username,
+                                  "fullname" : fullname,
+                                  "profileImageUrl" : profileImageUrl]
+                    
+                    // 딕셔너리를 만듬
+                    REF_USERS.updateChildValues(values) { (error, ref) in
+                        print("사용자 정보를 성공적으로 업데이트")
+                        //이 완료 블록에서 API 호출이 완료되고 성공합니다.
+                    }
+                    
             }
-            
-            print("성공적으로 사용자 등록")
-            
-            // 성공시 사용자에게 할당되는 고유의 uid를 가져옴, 데이터베이스에 저장할때 사용자 고유의 데이터를 관리하기 위함임
-            guard let uid = result?.user.uid else {return}
-            // 딕셔너리를 만듬
-            let values = ["email" : email, "username" : username, "fullname" : fullname]
-            
-            // 아래 명령 사용시 GoogleService-info.plist 파일에 데이터베이스 URL, API_KEY를 이용해 우리가 만든
-            // 파이어베이스 데이터베이스의 데이터를 읽고 씀
-            let ref =  Database.database().reference().child("users").child(uid)
-            // reference : 모든 데이터를 인터넷의 올바른 위치에 두는 문자열입니다.
-            // child("users").child(uid) 그런 다음 해당 점 하위 사용자로 이 사용자 구조를 만들었습니다.
-            // 우리가 만든 값으로 하위 값을 업데이트
-            ref.updateChildValues(values) { (error, ref) in
-                print("사용자 정보를 성공적으로 업데이트")
-                //이 완료 블록에서 API 호출이 완료되고 성공합니다.
-            }
+        }
+        
+       
             
         }
     }
---
+++ iOS_Twitter/iOS_Twitter/Utils/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  iOS_Twitter
+//
+//  Created by 정정욱 on 2023/07/17.
+//
+
+import FirebaseDatabase
+import FirebaseStorage
+
+// 파이어베이스에 빠르게 접근하기 위한 상수들을 정의, 생성
+let DB_REF = Database.database().reference()
+let REF_USERS = DB_REF.child("users")//해당 사용자 구조에 액세스하려고 할 때마다 참조가 될 것입니다.
+
+let STORAGE_REF = Storage.storage().reference() // 사용자 프로필 이미지는 FirebaseFirestore에 저장할것임
+let STORAGE_PROFILE_IMAGE = STORAGE_REF.child("profile_images")
+

7f250d199d8ad9fc1c36f55fccd2b8963738fecb
jeonguk29 <jeonguk29@naver.com>
2023-07-20 14:58:15 +0900

    사용자 프로필 이미지 불러오기 구현

--- iOS_Twitter/iOS_Twitter/Controller/FeedController.swift
+++ iOS_Twitter/iOS_Twitter/Controller/FeedController.swift
@@ -6,13 +6,16 @@
 //
 
 import UIKit
+import SDWebImage
 
 class FeedController: UIViewController{
     // MARK: - Properties
     
-    var user: User? { // 변경이 일어나면 아래 메세지를 출력
+    // 아래 이미지를 보여주는 코드가 실행되기 전에 해당 유저 데이터가 없을 수도 있음
+    // 따라서 기본적으로 프로필 이미지를 설정하기 전에 사용자가 설정되었는지 확인해야 합니다.
+    var user: User? { // 변경이 일어나면 아래 사용자 이미지 화면에 출력
         didSet {
-            print("DEBUG: Did set user in FeedController..")
+            configureLeftBarButton() // 해당 함수가 호출 될때는 사용자가 존재한다는 것을 알수 있음
         }
     }
     
@@ -32,10 +35,18 @@ class FeedController: UIViewController{
         imageView.contentMode = .scaleAspectFit
         navigationItem.titleView = imageView
         
+       
+    }
+    
+    func configureLeftBarButton(){
+        guard let user = user else {return}
+        
         let profileImageView = UIImageView()
-        profileImageView.backgroundColor = .twitterBlue
         profileImageView.setDimensions(width: 32, height: 32)
         profileImageView.layer.cornerRadius = 32 / 2
+        profileImageView.layer.masksToBounds = true
+        
+        profileImageView.sd_setImage(with: user.profileImageUrl, completed: nil)
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: profileImageView)
     }

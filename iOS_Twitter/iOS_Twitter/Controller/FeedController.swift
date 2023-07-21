d9554d45f869da0003ed2dec939d345f7ad8f5b7
jeonguk29 <jeonguk29@naver.com>
2023-07-21 18:29:40 +0900

    Tweet의 개수로 UICollectionView의 Cell 개수를 설정하고 didSet을 활용하여 collectionView를 reload

--- iOS_Twitter/iOS_Twitter/Controller/FeedController.swift
+++ iOS_Twitter/iOS_Twitter/Controller/FeedController.swift
@@ -22,6 +22,9 @@ class FeedController: UICollectionViewController{
         }
     }
     
+    private var tweets = [Tweet]() {
+        didSet {collectionView.reloadData()}
+    }
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -35,7 +38,7 @@ class FeedController: UICollectionViewController{
     // MARK: - API
     func fetchTweets(){
         TweetService.shared.fatchTweets { tweets in
-            print("DEBUG: Tweets are \(tweets)")
+            self.tweets = tweets
         }
     }
     
@@ -69,9 +72,23 @@ class FeedController: UICollectionViewController{
 
 }
 
+
+// MARK: - UICollectionViewDelegate/DataSource
+
 extension FeedController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        //print("DEBUG: Tweet count at time of collectionView function call is \(tweets.count)")
+        /*
+         private var tweets = [Tweet]() 으로만 코드 작성을 했을때
+         현제 0이 출력되는데 뷰가 로드되지마자 이함수가 호출 되기 때문임 뷰가 로드 될때는 tweets 배열이 빈 배열임
+         따라서 이 데이터 가져오기를 완료하고 결과로 이 트윗 배열을 실제로 설정하는 데 시간이 걸립니다.
+         그래서 화면에 보이는게 없지만 데이터를 가져와서 didSet을 통해 변경사항이 있을경우 리로드를하면 정상적으로 출력이 가능함
+         리로드시 확장으로 구현한 함수들은 다시 한번씩 호출 됨
+         +
+         이제 우리의 트윗 수는 2개가 될 것입니다.
+         따라서 두 개의 셀로 컬렉션 뷰를 다시 로드할 것입니다.
+         */
+        return tweets.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -81,6 +98,7 @@ extension FeedController {
     }
 }
 
+// MARK: - UICollectionViewDelegateFlowLayout
 extension FeedController: UICollectionViewDelegateFlowLayout {
     
     // 각 셀의 크기를 지정

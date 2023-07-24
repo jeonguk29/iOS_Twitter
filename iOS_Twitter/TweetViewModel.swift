d1001592ef7feb03e198f9a7b564d9359c61ad59
jeonguk29 <jeonguk29@naver.com>
2023-07-24 15:20:08 +0900

    Tweet 게시 시간 보여주기 timestamp

--- iOS_Twitter/TweetViewModel.swift
+++ iOS_Twitter/TweetViewModel.swift
@@ -17,6 +17,16 @@ struct TweetViewModel {
         return user.profileImageUrl
     }
     
+    var timestamp: String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.second, .minute, .hour, .day, .weekOfMonth]
+        formatter.maximumUnitCount = 1
+        formatter.unitsStyle = .abbreviated
+        let now = Date()
+        
+        //이것은 두 날짜 사이의 시차를 기반으로 형식이 지정된 문자열을 반환합니다.
+        return formatter.string(from: tweet.timestamp, to: now) ?? "2m"
+    }
     
     // 트윗 셀에서 작성하는 대신 원하는 효과를 여기 뷰모델에서 얻을수 있음 큐 클래스를 깨끗하게 유지할 수 있음
     var userInfoText: NSAttributedString {
@@ -25,6 +35,12 @@ struct TweetViewModel {
         title.append(NSAttributedString(string: "@\(user.username)",
                     attributes: [.font : UIFont.boldSystemFont(ofSize: 14),
                                             .foregroundColor: UIColor.lightGray]))
+        
+        title.append(NSAttributedString(string: " ・ \(timestamp)",
+                    attributes: [.font : UIFont.boldSystemFont(ofSize: 14),
+                                            .foregroundColor: UIColor.lightGray]))
+        
+
         return title
     }
     

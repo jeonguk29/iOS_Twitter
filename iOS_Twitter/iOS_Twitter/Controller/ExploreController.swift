a884abb316e80e5a3c80fd3263cb6f0252a54afe
jeonguk29 <jeonguk29@naver.com>
2023-08-01 19:38:57 +0900

    사용자 검색 Tap UI 설정

--- iOS_Twitter/iOS_Twitter/Controller/ExploreController.swift
+++ iOS_Twitter/iOS_Twitter/Controller/ExploreController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ExploreController: UIViewController{
+private let reuseIdentifier = "UserCell"
+
+class ExploreController: UITableViewController{
     // MARK: - Properties
     
-    
-    
+
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -23,6 +24,24 @@ class ExploreController: UIViewController{
     func configureUI() {
         view.backgroundColor = .white
         navigationItem.title = "Explore"
+        
+        //  재사용 셀에 재사용 식별자 등록
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none // 셀 사이에 구분선이 보이지 않게 설정
     }
 
+}
+
+extension ExploreController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as!
+        UserCell
+                
+        return cell
+    }
 }
---
+++ iOS_Twitter/iOS_Twitter/View/UserCell.swift
@@ -0,0 +1,60 @@
+//
+//  UserCell.swift
+//  iOS_Twitter
+//
+//  Created by 정정욱 on 2023/08/01.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    
+    
+    // MARK: - Properties
+    
+    private lazy var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 40, height: 40)
+        iv.layer.cornerRadius = 40/2
+        iv.backgroundColor = .twitterBlue
+        
+        return iv
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "Username"
+        return label
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "Fullname"
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+        let stack = UIStackView(arrangedSubviews: [usernameLabel, fullnameLabel])
+        stack.axis = .vertical
+        stack.spacing = 2
+        
+        addSubview(stack)
+        stack.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 12)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

bb6111e9834b4d55290d7a8feca73b3df1f5a939
jeonguk29 <jeonguk29@naver.com>
2023-08-21 15:35:29 +0900

    프로필 편집 : 뷰 모델로 셀 체우기2, 🔥직전 커밋은 데이터 모델을 만든 것임

--- iOS_Twitter/EditProfileViewModel.swift
+++ iOS_Twitter/EditProfileViewModel.swift
@@ -20,3 +20,42 @@ enum EditProfileOptions: Int, CaseIterable {
         }
     }
 }
+
+struct EditProfileViewModel {
+
+    private let user: User
+    let option: EditProfileOptions
+
+    var titleText: String {
+        return option.description
+    }
+
+    var optionValue: String? {
+        switch option {
+
+        case .fullname: return user.fullname
+        case .username: return user.username
+        case .bio: return user.bio
+        }
+    }
+
+    /*
+     옵션이 bio와 같으면 텍스트 필드를 숨기기.
+     따라서 옵션이 bio일 때 텍스트 필드를 숨기고 텍스트 보기를 표시 
+     bio와 같을때 텍스트 보기를 숨기고 텍스트 필드를 표시하려고 합니다.
+     */
+    var shouldHideTextField: Bool {
+        return option == .bio
+    }
+
+    var shouldHideTextView: Bool {
+        return option != .bio
+    }
+
+    //이 뷰 모델은 데이터를 설정하기 위해 이 두 가지가 모두 필요
+    // 각 셀의 title 제목을 넣기 위해 + 제목에 해당하는 실제 사용자 값을 넣기 위해
+    init(user: User, option: EditProfileOptions) {
+        self.user = user
+        self.option = option
+    }
+}

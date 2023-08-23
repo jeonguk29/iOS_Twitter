5fce5a90dd96a6221f407134010590cd57716961
jeonguk29 <jeonguk29@naver.com>
2023-08-23 17:44:26 +0900

    알림가져오기 : 버그수정 => 알림 없을때 무한 로딩 해결

--- iOS_Twitter/iOS_Twitter/API/NotificationService.swift
+++ iOS_Twitter/iOS_Twitter/API/NotificationService.swift
@@ -37,22 +37,44 @@ struct NotificationService {
     
     // 알림 가져오는 메서드
     func fetchNotifications(completion: @escaping([Notification]) -> Void) {
-            var notifications = [Notification]() // 알림을 담을 배열
-
-            guard let uid = Auth.auth().currentUser?.uid else { return } // 현제 사용자 uid 뽑기
-
-        // 현재 사용자의 uid를 기준으로 모든 알림들 가져오기
-        REF_NOTIFICATIONS.child(uid).observe(.childAdded) { (snapshot,err)  in
-                guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
-                guard let uid = dictionary["uid"] as? String else { return }
-                
-                // 알림에 있는 uid와 연결된 사용자를 가져오기 (누가 좋아요를 눌렀고, 팔로우를 했는지 알림을 보낸 사람 정보를 가져오기 위함)
-                UserService.shared.fetchUser(uid: uid) { (user) in
-                    let notification = Notification(user: user, dictionary: dictionary)
-                    notifications.append(notification)
-                    completion(notifications)
-                }
+        var notifications = [Notification]() // 알림을 담을 배열
+        
+        guard let uid = Auth.auth().currentUser?.uid else { return } // 현제 사용자 uid 뽑기
+        
+        
+        // 사용자에 대한 알림이 있는지 확인
+        REF_NOTIFICATIONS.child(uid).observeSingleEvent(of: .value) { snapshot in
+            if !snapshot.exists() {
+                // 사용자에게 알림이 없음을 의미
+                completion([])
+            } else {
+                getNotifications(uid: uid, completion: completion)
             }
         }
+    }
+    
+    
+    fileprivate func getNotifications(uid: String,
+                                      completion: @escaping([Notification]) -> Void) {
+        var notifications = [Notification]()
+        
+        // 현재 사용자의 uid를 기준으로 모든 알림들 가져오기
+        REF_NOTIFICATIONS.child(uid).observe(.childAdded) { (snapshot,err)  in
+            /*
+             알림이 없을때 빙빙 도는 것은 해당 블록을 수행 할 수 없어서
+             마지막   completion(notifications)을 실행 할수 없기 때문임
+             */
+            
+            guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
+            guard let uid = dictionary["uid"] as? String else { return }
+            
+            // 알림에 있는 uid와 연결된 사용자를 가져오기 (누가 좋아요를 눌렀고, 팔로우를 했는지 알림을 보낸 사람 정보를 가져오기 위함)
+            UserService.shared.fetchUser(uid: uid) { (user) in
+                let notification = Notification(user: user, dictionary: dictionary)
+                notifications.append(notification)
+                completion(notifications)
+            }
+        }
+    }
 
 }
--- iOS_Twitter/iOS_Twitter/Controller/NotificationsController.swift
+++ iOS_Twitter/iOS_Twitter/Controller/NotificationsController.swift
@@ -50,13 +50,18 @@ class NotificationsController: UITableViewController {
     
     // 사용자를 팔로우 하는지 판단하는 함수 : 리팩토링
     func checkIfUserIsFollowed(notifications: [Notification]) {
-        for (index, notification) in notifications.enumerated() {
-            if case .follow = notification.type {
-                let user = notification.user
-                
-                UserService.shared.checkIfUserIsFollowd(uid: user.uid) { isFollowed in
+        guard !notifications.isEmpty else { return }
+        
+        notifications.forEach { notification in
+            guard case .follow = notification.type else { return }
+            
+            let user = notification.user
+            
+            UserService.shared.checkIfUserIsFollowd(uid: user.uid) { isFollowed in
+                if let index = self.notifications.firstIndex(where: { $0.user.uid == notification.user.uid }) {
                     self.notifications[index].user.isFollowed = isFollowed
                 }
+                
             }
         }
     }
